e4b4e54ed70e81cca4ab044df0a25a0fafebf18c
trueeh-slave <steamtruesnowace@gmail.com>
2025-03-12 21:55:46 -0500

    sonar properties

--- academy-service/src/main/java/co/edu/gimnasiolorismalaguzzi/academyservice/student/service/persistence/PersistenceGroupStudentPort.java
+++ academy-service/src/main/java/co/edu/gimnasiolorismalaguzzi/academyservice/student/service/persistence/PersistenceGroupStudentPort.java
@@ -2,7 +2,6 @@ package co.edu.gimnasiolorismalaguzzi.academyservice.student.service.persistence
 
 import co.edu.gimnasiolorismalaguzzi.academyservice.common.PersistencePort;
 import co.edu.gimnasiolorismalaguzzi.academyservice.student.domain.GroupStudentsDomain;
-import co.edu.gimnasiolorismalaguzzi.academyservice.student.domain.StudentPromotionDTO;
 
 import java.util.List;
 
@@ -12,8 +11,4 @@ public interface PersistenceGroupStudentPort extends PersistencePort<GroupStuden
     List<GroupStudentsDomain> getGroupsStudentByGroupId(Integer groupId, String statusNotLike);
 
     List<GroupStudentsDomain> getListByMentorIdByYear(Integer mentorId, Integer year);
-
-    List<GroupStudentsDomain> getGroupListByStatus(String status);
-
-    List<GroupStudentsDomain> promoteStudents(StudentPromotionDTO promotionDTO);
 }
